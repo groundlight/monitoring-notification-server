eb93f5ecb8be9dd78f60d7b43ff53161f33a8ed6
Max McKelvey <maxmckelvey@Maxs-MBP.positronix.local>
2023-07-14 08:45:11 -0700

    removed useless comments

--- api/gl_process.py
+++ api/gl_process.py
@@ -6,29 +6,6 @@ import groundlight
 import framegrab
 import pydantic
 import multiprocessing
-# from api.index import Detector as PYDetector
-
-class TriggerType(Enum):
-    MOTION = 1
-    TIME = 2
-    PIN = 3
-
-class Trigger():
-    _type: TriggerType
-
-class MotionTrigger(Trigger):
-    _type = TriggerType.MOTION
-
-@dataclass
-class TimeTrigger(Trigger):
-    cycle_time: int
-    _type = TriggerType.TIME
-
-@dataclass
-class PinTrigger(Trigger):
-    pin: int
-    _type = TriggerType.PIN
-
 
 # class Detector(Trigger):
 #     id: str
@@ -44,62 +21,42 @@ class PinTrigger(Trigger):
 #         elif trigger_type == "pin":
 #             self.trigger = PinTrigger(pin)
 
-class Config(pydantic.BaseModel):
-    vid_config: dict
-    image: str
-    trigger_type: str
-    cycle_time: int | None
-    pin: int | None
-    pin_active_state: int | None
+# class Config(pydantic.BaseModel):
+#     vid_config: dict
+#     image: str
+#     trigger_type: str
+#     cycle_time: int | None
+#     pin: int | None
+#     pin_active_state: int | None
 
-class Detector(pydantic.BaseModel):
-    name: str
-    id: str
-    query: str
-    config: Config
+# class Detector(pydantic.BaseModel):
+#     name: str
+#     id: str
+#     query: str
+#     config: Config
 
-class DetectorList(pydantic.BaseModel):
-    detectors: list[Detector]
+# class DetectorList(pydantic.BaseModel):
+#     detectors: list[Detector]
 
-# def run_process(detector: Detector, api_key: str, endpoint: str):
-# def run_process(detector: dict, api_key: str, endpoint: str):
 def run_process(detector: dict, api_key: str, endpoint: str, notify_queue: multiprocessing.Queue, photo_queue: multiprocessing.Queue):
     print("Starting process...")
 
-    # trigger = detector.trigger
-    # trigger = detector.config.trigger_type
-    # vid_src = detector.vid_src
-    # grabbers = framegrab.FrameGrabber.autodiscover()
-    # keys  = list(grabbers.keys())
-    # vid = grabbers[keys[vid_src]]
-    # for k, v in grabbers.items():
-    #     config = v.config
-    #     if "input_type" in config and config["input_type"] == "webcam":
-    #         v.config["idx"] = v.idx
-
-    # print(detector)
-    # print(detector.keys())
-    # print(detector["config"].keys())
-    
-    # vid = framegrab.FrameGrabber.create_grabber(detector["config"]["vid_config"])
-
-    # for v in grabbers.values():
-    #     v.release()
-    # vid = grabbers[vid_src]["grabber"]
-    # vid = framegrab.grabber.FrameGrabber.create_grabber(config)
-
-    # trigger_type = trigger._type
     trigger_type = detector["config"]["trigger_type"]
 
     delay = lambda: time.sleep(30)
 
-    if trigger_type == TriggerType.MOTION:
-        return # TODO: implement
-    elif trigger_type == TriggerType.TIME or trigger_type == "time":
+    if trigger_type == "motion":
+        # TODO: implement
+        raise ValueError(f"Trigger type [{trigger_type}] not yet supported.")
+    elif trigger_type == "time":
         # delay = lambda: time.sleep(trigger.cycle_time)
         delay = lambda: time.sleep(detector["config"]["cycle_time"])
-    elif trigger_type == TriggerType.PIN:
-        return # TODO: implement
+    elif trigger_type == "pin":
+        # TODO: implement
+        raise ValueError(f"Trigger type [{trigger_type}] not yet supported.")
+    elif trigger_type == "http":
+        # TODO: implement
+        raise ValueError(f"Trigger type [{trigger_type}] not yet supported.")
     else:
         raise ValueError(f"Invalid trigger type: {trigger_type}")
     
@@ -110,10 +67,7 @@ def run_process(detector: dict, api_key: str, endpoint: str, notify_queue: multi
 
     print(f"Starting detector {detector['id']}...")
 
-    # exit()
-
     while(True):
-        # frame = vid.grab()
         notify_queue.put("fetch")
         frame = photo_queue.get(timeout=30)
         # if not frame:
--- api/index.py
+++ api/index.py
@@ -1,12 +1,12 @@
 import multiprocessing
 import time
 from typing import List
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI
 import json
 import yaml
 import groundlight
 import pydantic
-from api.gl_process import Detector as GLDetector
+# from api.gl_process import Detector as GLDetector
 from api.gl_process import run_process
 import framegrab
 from framegrab import FrameGrabber
@@ -39,8 +39,6 @@ app.DETECTOR_PHOTO_QUEUES: List[multiprocessing.Queue] = []
 app.DETECTOR_GRAB_NOTIFY_QUEUES: List[multiprocessing.Queue] = []
 app.DETECTOR_CONFIG = {}
 
-# app.DETECTOR_VID_CONFIGS = []
-
 def start_processes(api_key, endpoint, detectors):
     app.DETECTOR_CONFIG = {
         "api_key": api_key,
@@ -48,13 +46,6 @@ def start_processes(api_key, endpoint, detectors):
         "detectors": detectors,
     }
     detectors = list(filter(lambda d: d["config"]["enabled"], detectors))
-    # app.DETECTOR_PROCESSES = [multiprocessing.Process(target=run_process, args=(
-    #     d,
-    #     api_key,
-    #     endpoint,
-    # )) for d in detectors]
-    # for p in app.DETECTOR_PROCESSES:
-    #     p.start()
     app.DETECTOR_PHOTO_QUEUES = [multiprocessing.Queue(1) for _ in detectors]
     app.DETECTOR_GRAB_NOTIFY_QUEUES = [multiprocessing.Queue(1) for _ in detectors]
     app.DETECTOR_PROCESSES = []
@@ -68,38 +59,6 @@ def start_processes(api_key, endpoint, detectors):
         ))
         app.DETECTOR_PROCESSES.append(process)
         process.start()
-
-
-# def make_grabbers():
-#     try:
-#         for p in app.DETECTOR_PROCESSES:
-#             p.terminate()
-#     except:
-#         pass
-
-#     grabbers = framegrab.FrameGrabber.autodiscover()
-#     cameras = []
-#     for k, v in grabbers.items():
-#         config = v.config
-#         if "input_type" in config and config["input_type"] == "webcam":
-#             v.config["idx"] = v.idx
-
-#         img = v.grab()
-#         v.release()
-#         _, jpg = cv2.imencode('.jpg', img)
-#         jpg_64 = base64.b64encode(jpg)
-        
-#         cameras.append({"config": config, "image": jpg_64})
-    
-#     try:
-#         api_key = app.DETECTOR_CONFIG["api_key"]
-#         endpoint = app.DETECTOR_CONFIG["endpoint"]
-#         detectors = app.DETECTOR_CONFIG["detectors"]
-#         start_processes(api_key, endpoint, detectors)
-#     except:
-#         print("Failed to start processes")
-    
-#     return cameras
 
 def make_grabbers():
     grabbers: List[framegrab.grabber.WebcamFrameGrabber | framegrab.grabber.BaslerUSBFrameGrabber | framegrab.grabber.RealSenseFrameGrabber | framegrab.grabber.RTSPFrameGrabber] = []
@@ -123,9 +82,6 @@ def make_grabbers():
             print(f"Failed to create framegrabber for {d['name']}")
     
     return grabbers
-
-# app.ALL_GRABBERS = make_grabbers()
-
 
 print("Loading config...")
 try:
@@ -236,53 +192,6 @@ def post_api_key(key: ApiKey):
         json.dump(config, f, indent=4)
     return config
 
-# @app.get("/api/cameras")
-# @app.get("/api/refresh-cameras")
-# def refresh_cameras():
-#     app.ALL_GRABBERS = make_grabbers()
-#     return app.ALL_GRABBERS
-
-# @app.post("/api/refresh-camera")
-# def refresh_camera(config: dict):
-#     for c in app.ALL_GRABBERS:
-#         if c["config"] == config:
-#             # stop processes
-#             indexes_to_suspend = []
-#             if "detectors" in app.DETECTOR_CONFIG:
-#                 detectors = app.DETECTOR_CONFIG["detectors"]
-#                 for i in range(len(app.DETECTOR_CONFIG["detectors"])):
-#                     if detectors[i]["config"] == config and detectors[i]["config"]["enabled"]:
-#                         indexes_to_suspend.append(i)
-#             for i in indexes_to_suspend:
-#                 print(i)
-#                 app.DETECTOR_PROCESSES[i].terminate()
-
-#             try:
-#                 v = framegrab.FrameGrabber.create_grabber(config)
-#             except:
-#                 continue
-#             time.sleep(0.5)
-#             _, jpg = cv2.imencode('.jpg', v.grab())
-#             jpg_64 = base64.b64encode(jpg)
-#             c["image"] = jpg_64
-#             v.release()
-
-#             # restart processes
-#             for i in indexes_to_suspend:
-#                 app.DETECTOR_PROCESSES[i] = multiprocessing.Process(target=run_process, args=(
-#                     app.DETECTOR_CONFIG["detectors"][i],
-#                     app.DETECTOR_CONFIG["api_key"],
-#                     app.DETECTOR_CONFIG["endpoint"],
-#                 ))
-#                 app.DETECTOR_PROCESSES[i].start()
-
-#             return c
-#     return None
-
-# @app.get("/api/cameras")
-# def get_cameras():
-#     return app.ALL_GRABBERS
-
 @app.post("/api/refresh-camera")
 def refresh_camera(config: dict):
     for g in app.ALL_GRABBERS:
@@ -294,21 +203,6 @@ def refresh_camera(config: dict):
 @app.get("/api/cameras")
 def get_cameras():
     return [{"config": g.config, "image": base64.b64encode(cv2.imencode(".jpg", g.grab())[1])} for g in app.ALL_GRABBERS]
-
-# @app.get("/api/cameras/name:{name}")
-# def get_camera(name: str):
-#     # for c in app.ALL_GRABBERS:
-#     #     if c["name"] == name:
-#     #         return {"name": c["name"], "image": c["image"]}
-#     return None
-
-# @app.get("/api/cameras/idx:{idx}")
-# def get_camera(idx: int):
-#     # for c in app.ALL_GRABBERS:
-#     #     if c["name"] == name:
-#     #         return {"name": c["name"], "image": c["image"]}
-#     return None
-# @app.
 
 async def test():
     while True:
@@ -326,11 +220,5 @@ async def test():
 
 @app.on_event("startup")
 async def app_startup():
-    # asyncio.create_task(test())
-    # background_tasks.add_task(test)
     loop = asyncio.get_event_loop()
     loop.create_task(test())
-    # pass
-
-# bgt = BackgroundTasks()
-# bgt.add_task(test, app)
